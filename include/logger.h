9c955c86c7018c3433136caac07392650096b6ef
Christopher Capobianco <ccapo.astro@gmail.com>
2017-02-25 15:34:50 +0000

    logger.h edited online with Bitbucket

--- include/logger.h
+++ include/logger.h
@@ -1,16 +1,17 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+/* Logging Tags */
 #define DEBUG   "  [debug]"
 #define INFO    "   [info]"
 #define NOTICE  " [notice]"
 #define WARNING "[warning]"
 #define ERROR   "  [error]"
 
-// Messages will be appended to this file
+/* Messages will be appended to this file */
 #define LOGFILE	"log/habpi.log"
 
-// Keeps track whether the log file is exists
+/* Keeps track of whether the log file exists */
 extern int log_file_exists;
 
 void logger(const char *tag, const char *message);
